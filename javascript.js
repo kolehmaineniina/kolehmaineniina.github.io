480796adf42aac2e79e51e47942915799fb611bd
kolehmaineniina@gmail.com <kolehmaineniina@gmail.com>
2024-10-13 00:17:07 +0300

    getting there

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
 function toggleExpand(element) {
     element.classList.toggle('expanded');
   }
+
+
+  document.getElementById('hamburger').addEventListener('click', function(){var navLinks = document.getElementById('nav-links');navLinks.classList.toggle('active');});
+  
+
