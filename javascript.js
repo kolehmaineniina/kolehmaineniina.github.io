ccb10c6a7249e27719a8408cc61d25b821f4dd67
kolehmaineniina@gmail.com <kolehmaineniina@gmail.com>
2024-10-13 14:38:57 +0300

    hamburger menu

--- javascript.js
+++ javascript.js
@@ -2,7 +2,11 @@ function toggleExpand(element) {
     element.classList.toggle('expanded');
   }
 
+document.getElementById('hamburger').addEventListener('click', function(){var navLinks = document.getElementById('nav-links');navLinks.classList.toggle('active');});
 
-  document.getElementById('hamburger').addEventListener('click', function(){var navLinks = document.getElementById('nav-links');navLinks.classList.toggle('active');});
-  
+document.getElementById("hamburger-menu").addEventListener("click", function() {
+  const navLinks = document.getElementById("nav-links");
+  navLinks.classList.toggle("active");
+});
+
 
