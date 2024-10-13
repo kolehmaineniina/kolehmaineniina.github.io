bc228a75dd54beb35fe62aa0449cf547b4d2ced3
kolehmaineniina@gmail.com <kolehmaineniina@gmail.com>
2024-10-13 14:50:14 +0300

    nav links update

--- javascript.js
+++ javascript.js
@@ -4,9 +4,5 @@ function toggleExpand(element) {
 
 document.getElementById('hamburger').addEventListener('click', function(){var navLinks = document.getElementById('nav-links');navLinks.classList.toggle('active');});
 
-document.getElementById("hamburger-menu").addEventListener("click", function() {
-  const navLinks = document.getElementById("nav-links");
-  navLinks.classList.toggle("active");
-});
 
 
